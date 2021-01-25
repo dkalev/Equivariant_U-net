5a7b0130ca18da005394884788958cd644e0d85b
Damyan Kalev <dzkalev@gmail.com>
2021-01-25 22:30:44 +0100

    Updated slurm job

--- job.sh
+++ job.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-#SBATCH -N 2 # Number of nodes
-#SBATCH -t 04:00:00 # excepted wall clock time
-#SBATCH -p gpu_shared # specify partition
+#SBATCH --job-name="DRIVE_EquivUNet"
+#SBATCH --nodes=1 # Number of nodes
+#SBATCH --cpus-per-task=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=04:00:00 # excepted wall clock time
+#SBATCH --partition=gpu_shared # specify partition
 
 module load 2020
 module load Anaconda3/2020.02
 module load CUDA/11.0.2-GCC-9.3.0
 
-cp -r $HOME/DRIVE/training "$TMPDIR" # copy data to scratch
+cp -r $HOME/DRIVE "$TMPDIR" # copy data to scratch
 
-python $HOME/DRIVE/train.py --output_dir="$TMPDIR"
+cd "$TMPDIR"/DRIVE
 
-cp -r "$TMPDIR"/models $HOME/DRIVE
+python train.py
 
+cp -r lightning_logs/* $HOME/DRIVE/lightning_logs
 echo DONE
