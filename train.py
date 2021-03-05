8d104d5cb30f105e16fa60a4b4e9d9238d230e59
Damyan Kalev <dzkalev@gmail.com>
2021-03-05 16:24:22 +0100

    Updates to train script

--- train.py
+++ train.py
@@ -1,60 +1,71 @@
-from models import C4UNet, UNet, HarmonicUNet
+import argparse
+from models import C4UNet, UNet, HarmonicUNet, SteerableCNN
 from e2cnn import gspaces
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
-from test_tube import Experiment, HyperOptArgumentParser
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from dataset import RetinalDataModule
-from torch.nn import BCEWithLogitsLoss
 
 def get_model(config):
-    if config.model == 'discrete':
-        r2_act = gspaces.Rot2dOnR2(8)
-        return C4UNet(r2_act, config.in_channels, config.out_channels, n_features=config.n_features, lr=config.lr)
-    elif config.model == 'standard':
-        return UNet(config.in_channels, config.out_channels, n_features=config.n_features, lr=config.lr)
-    elif config.model == 'harmonic':
-        r2_act = gspaces.Rot2dOnR2(-1, maximum_frequency=1)
-        return HarmonicUNet(r2_act, config.in_channels, config.out_channels, n_features=config.n_features, lr=config.lr)
+    if config['model'] == 'discrete':
+        if config['group'] == 'circle':
+            gspace = gspaces.Rot2dOnR2(config['N'])
+        elif config['group'] == 'dihedral':
+            gspace = gspaces.FlipRot2dOnR2(config['N'])
+        else:
+            raise ValueError('Discrete group argument "group" must be one of ["circle", "dihedral"]')
+        return C4UNet(gspace, config['in_channels'], config['out_channels'], n_features=config['n_features'], lr=config['lr'])
+    elif config['model'] == 'standard':
+        return UNet(config['in_channels'], config['out_channels'], n_features=config['n_features'], lr=config['lr'])
+    elif config['model'] == 'harmonic':
+        gspace = gspaces.Rot2dOnR2(-1, maximum_frequency=config['max_freq'])
+        return HarmonicUNet(gspace, config['in_channels'], config['out_channels'], n_features=config['n_features'], lr=config['lr'])
+    elif config['model'] == 'steerable':
+        gspace = gspaces.Rot2dOnR2(-1, maximum_frequency=config['max_freq'])
+        return SteerableCNN(gspace,
+                        config['in_channels'],
+                        config['out_channels'],
+                        n_blocks=config['n_blocks'],
+                        n_features=config['n_features'],
+                        irrep_type=config['irrep_type'],
+                        lr=config['lr'])
     else:
-        raise ValueError(f'Unsupported model type: {config.model}')
+        raise ValueError(f'Unsupported model type: {config["model"]}')
 
 def get_exp_name(config):
-    return f'{config.model}_{config.n_features}_{config.lr}_{config.batch_size}' 
+    return f"{config['model']}_{config['n_features']}_{config['lr']}_{config['batch_size']}" 
 
 def train(config):
     rdm = RetinalDataModule()
 
     model = get_model(config)
-    model.crit = BCEWithLogitsLoss()
-    checkpoint_cb = ModelCheckpoint(monitor='valid_loss')
-    logger = TensorBoardLogger('logs', name=get_exp_name(config))
-    trainer = pl.Trainer(gpus=1, max_epochs=config.n_epochs, logger=logger, callbacks=[checkpoint_cb])
+    logger = TensorBoardLogger('logs', name=get_exp_name(config), default_hp_metric=False)
+    logger.log_hyperparams(config, {
+        'train_acc': 0,
+        'train_f1': 0,
+        'train_loss': 0,
+        'valid_acc': 0,
+        'valid_f1': 0,
+        'valid_loss': 0,
+        })
+    trainer = pl.Trainer(gpus=1, max_epochs=config['n_epochs'], logger=logger, callbacks=[
+        EarlyStopping(monitor='valid_loss', patience=10, mode='min'),
+        ModelCheckpoint(monitor='valid_loss')])
     trainer.fit(model, rdm)
 
-def grid_search(config):
-    rdm = RetinalDataModule()
-    rdm.prepare_data()
-    rdm.setup()
-
-    exp = Experiment(name=get_exp_name(config), debug=False, save_dir='test_tube')
-    for hparam in config.trials(10):
-        model = get_model(hparam)
-        train_loader = rdm.train_dataloader(batch_size=config.batch_size)
-        valid_loader = rdm.val_dataloader(batch_size=config.batch_size)
-        trainer = pl.Trainer(gpus=1, max_epochs=10)
-        trainer.fit(model, train_loader, valid_loader)
-        exp.log(trainer.logged_metrics)
-
-    exp.save()
 
 if __name__ == '__main__':
-    parser = HyperOptArgumentParser(description="Train a U-Net model", strategy='grid_search')
-    parser.add_argument('--grid_search', default=False, help='Grid search')
-    parser.add_argument('--model', type=str, default='discrete', help='Model type. One of [discrete, standard, harmonic]')
-    parser.opt_range('--lr', default=1e-3, type=float, low=1e-6, high=1e-1, nb_samples=10, tunable=True)
-    parser.opt_list('--n_features', default=32, type=int, tunable=True, options=[8, 16, 32, 64])
-    parser.opt_list('--batch_size', type=int, default=4, options=[4, 8, 16], help='Batch size')
+    parser = argparse.ArgumentParser(description="Train a U-Net model")
+    parser.add_argument('--model', type=str, default='discrete', help='Model type. One of [discrete, standard, harmonic, steerable]')
+    parser.add_argument('--irrep_type', type=str, default='all', help='Steerable model irrep type. One of [all, even, odd]')
+    parser.add_argument('--lr', default=1e-3, type=float, help='Learning rate for the Adam optimizer')
+    parser.add_argument('--N', default=4, type=int, help='N for circle and dihedral groups')
+    parser.add_argument('--group', type=str, default='circle', help='Type of discrete group. One of [circle, dihedral]')
+    parser.add_argument('--n_features', type=int, default=32, help='Number of features per block')
+    parser.add_argument('--n_blocks', type=int, default=10, help='Number of conv blocks for steerable model')
+    parser.add_argument('--max_freq',  type=int, default=2,help='Max frequency for steerable model')
+    parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
     parser.add_argument('--n_epochs', type=int, default=10, help='Max number of epochs')
     parser.add_argument('--in_channels', type=int, default=3, help='Number of input channels')
     parser.add_argument('--out_channels', type=int, default=1, help='Number of output channels')
@@ -65,10 +76,8 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', type=str, default='.', help='Output directory path to save results')
 
     config = parser.parse_args()
+    config = vars(config)
 
-    if config.grid_search:
-        grid_search(config)
-    else:
-        train(config)
+    train(config)
 
 
